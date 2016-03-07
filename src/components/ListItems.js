3a87db887bcd94967fba7b13e9fa573efaaa4e3f
Justin <juns3700@gmail.com>
2016-03-07 15:03:01 +0800

    Add demo image

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -17,15 +17,11 @@ class ListItems extends Component {
                 <div className="item-tools">
                     <button className="button del">X</button>
                 </div>
-                <div className="item-id"># {id + 1}
+                <div className="item-id"># {id+1}
                     <span className="item-user-name">{user}</span> : 
                 </div>
-                <div className="item-msg">
-                    { message }
-                </div>
-                <div className="item-time">
-                    { time }
-                </div>
+                <div className="item-msg">{message}</div>
+                <div className="item-time">{time}</div>
             </li>
         );
     };
